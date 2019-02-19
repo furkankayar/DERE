372ca1648d32d372ed96047430ea540592cd55dc
Furkan Kayar <furkankayar427@hotmail.com>
2019-02-19 22:13:18 +0300

    Customer created and Agent is inherited from Customer

--- Agent.java
+++ Agent.java
@@ -1,51 +1,24 @@
 
 
-public class Agent{
+public class Agent extends Customer{
 
-  private int id;
-  private String name;
   private Agency agency;
-  private String birthdate;
-  private String address;
-  private String phone;
-  private String gender;
   private double salary;
 
   private static int autoincrement = 1;
 
   public Agent(String name, Agency agency, String birthdate,
                 String address, String phone, String gender, double salary ){
-
-    this.name = name;
+                  
+    super(autoincrement++, name, birthdate, address, phone, gender);
     this.agency = agency;
-    this.birthdate = birthdate;
-    this.address = address;
-    this.phone = phone;
-    this.gender = gender;
     this.salary = salary;
-    this.id = autoincrement++;
+
   }
 
-  public int getId(){
-    return this.id;
-  }
-  public String getName(){
-    return this.name;
-  }
+
   public Agency getAgency(){
     return this.agency;
-  }
-  public String getBirthdate(){
-    return this.birthdate;
-  }
-  public String getAddress(){
-    return this.address;
-  }
-  public String getPhone(){
-    return this.phone;
-  }
-  public String getGender(){
-    return this.gender;
   }
   public double getSalary(){
     return this.salary;
